d4647cac21b137dae64a3b300d7d95f412b7f30d
Ahmed Mohamed <ahmedkomeha18@gmail.com>
2024-03-12 00:17:33 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const DalelApp());
+}
+
+class DalelApp extends StatelessWidget {
+  const DalelApp({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+     
+     
+        
+    
+    );
+  }
+}
+
